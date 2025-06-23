4f05c97908108f4f313d9c5881ce99aea5679418
DennisWu93 <rongzong.wu@alumni.anu.edu.au>
2025-06-23 19:39:19 +0800

    git commit -am "Django 0623"

--- tasks/views.py
+++ tasks/views.py
@@ -4,13 +4,16 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-tasks = ['foo', 'bar', 'baz']
+#can create tasks when request, then mute below line and add lines 14-15
+# tasks = ['foo', 'bar', 'baz']
 
 class NewTaskForm(forms.Form):
     task = forms.CharField(label='New Task', max_length=100)
     priority = forms.IntegerField(label='Priority', min_value=1, max_value=10)
 def index(request):
-    return render(request, "tasks/index.html",{"tasks": tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html",{"tasks": request.session["tasks"]})
 
 def add(request):
     #if the user submits data
